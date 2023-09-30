2d2af170b76633bd10ff678106eb06b8f15416a8
Charan <manicharan150@gmail.com>
2023-09-30 17:20:27 +0530

    Addind the package files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='My_Calculator',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here
+    ],
+)
